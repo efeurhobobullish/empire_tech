4910e95cc72fb6f84013514d14b571ef6aaddd88
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-09-12 12:12:46 -1100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
 /*=============== HOME SPLIT TEXT ===============*/
-const text = document.querySelector('.home__split-text');
+const { animate, text, stagger } = anime
+
+const { chars: chars1 } = text.split('.home__profession-1', {chars: true });
+const { chars: chars2 } = text.split('.home__profession-2', {chars: true });
+
+animate(chars1, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 4000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
 
 /*=============== SWIPER PROJECTS ===============*/
 const swiper = new Swiper('.projects__container', {
